e4b466f404ddb0472fc6b05e295cc787cec3134b
ARHoss <rhossaincse@gmail.com>
2022-10-15 19:42:32 -0600

    still trying to fix

--- admin/categories.php
+++ admin/categories.php
@@ -78,21 +78,92 @@
                             </form>
 
                             <!-- Update data form -->
-                            <?php 
-                            
-                            if(isset($_GET['edit'])){
 
-                                // Retreiving data from GET
-                                $cat_id = $_GET['edit'];
-                                
+                            <form action="categories.php" method="post">
 
-                                include "includes/update_categories.php";  
+<div class="form-group">
+    <label for="cat_title">Edit Category</label>
 
-                            }
-                            
-                            
-                            
-                            ?>
+    <!-- Query to populate the edit category form placeholder -->
+    <?php 
+    
+        
+        if(isset($_GET['edit'])){// Retreiving data from post
+            $cat_id = $_GET['edit'];
+            
+            
+            
+            
+            $select_categories_id = "SELECT * FROM categories WHERE cat_id= $cat_id";
+            $select_categories = mysqli_query($connection, $select_categories_id);
+
+            while($row = mysqli_fetch_assoc($select_categories)){
+            
+            $cat_title = $row['cat_title'];
+            $cat_id = $row['cat_id'];
+            
+
+
+
+            
+    ?>                                           
+        <!-- The inpute where the vaulue needs to be passed -->
+        <input value=<?php if(isset($cat_title)){echo $cat_title;} ?> class="form-control" type="text" name="cat_title">
+        
+        
+    <?php 
+
+            }
+        }
+    
+
+    
+    ?>
+    <!-- Query to populate the edit category form placeholder -ends -->
+
+    <!-- Queryt to update category title -->
+
+    <?php 
+    $cat_id = 3;
+    
+    // Updates data
+    if(isset($_POST['update_category'])){
+    
+        // Retreiving data from post
+        $the_cat_title = $_POST['cat_title'];
+
+        echo $_POST['update_category'];
+        
+        
+        
+                                 
+
+                // insert values
+                $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id} ";
+                // $query .= "cat_title = '{$the_cat_title}' ";
+                // $query .= "WHERE cat_id = {$cat_id} ";
+        
+                $update_query = mysqli_query($connection, $query);
+                if(!$update_query){
+                    die("QUERY FAILED" . mysqli_error($connection));
+                }else{
+                    echo "record updated";
+                }
+
+
+    }else{echo "Not Working";}
+           
+    ?>
+    
+    <!-- Queryt to update category title ends-->
+    
+</div>
+<div class="form-group">
+    <!-- changed value of submit to create another post object -->
+    <input class="btn btn-primary" type="submit" name="update_category" value="update_category">
+</div>
+
+</form>
                             
 
                         </div>
