6d6e77536f482afe659dbb7f7e87c2b8ef1cbc15
ARHoss <rhossaincse@gmail.com>
2022-10-15 21:51:58 -0600

    working

--- admin/categories.php
+++ admin/categories.php
@@ -46,16 +46,11 @@
                             <!-- Update data form -->
                             <?php 
                             
-                            if(isset($_GET['edit'])){
-
-                            
-                                
+                            if(isset($_GET['edit'])){ 
 
                                 include "includes/update_categories.php";  
 
                             }
-                            
-                            
                             
                             ?>
                             
@@ -78,64 +73,12 @@
     
                                 <tbody>                                    
                                     <!-- DB Query to read All data to table-->
-                                    <?php   
-                
-                                        $query = "SELECT * FROM categories";
-                                        $select_categories = mysqli_query($connection, $query);
-
-                                        while($row = mysqli_fetch_assoc($select_categories)){
-                                            
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
-
-                                            echo "<tr>";
-                                            echo "<td>{$cat_id}</td>";
-                                            echo "<td>{$cat_title}</td>";
-
-                                            // Delete - Passing commant to GET - array returned - ([delete]=>$cat_id) 
-                                            echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-
-                                            // Edit - Passing commant to GET - array returned - ([delete]=>$cat_id) 
-                                            echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                            
-                                            echo "</tr>";
-                                        }
-                                            
-                                    ?>
+                                    <?php  findAllCategories();  ?>
                                     <!-- DB Query ends--> 
                                 </tbody>
 
                                 <!-- DB Query to Delete Data -->
-                                <?php 
-                                
-                                // _Get looking for 'delete' key
-                                if(isset($_GET['delete'])){
-
-    
-                            
-                                    // Retreiving data from post
-                                    $the_cat_id = $_GET['delete'];
-                                    
-                            
-                            
-                                    $query = "DELETE FROM categories ";
-                                    $query .= "WHERE cat_id = {$the_cat_id} ";
-                                    $delete_query = mysqli_query($connection, $query);
-                                    
-                                    // Checks if the query fails - not printing if works - need to fix
-                                    // if(!$deletes_query){
-                                    //     die("QUERY FAILED" . mysqli_error($connection));
-                                    // }else{
-                                    //     echo "<font color='blue'>".$myvariable."</font>";
-                                    // }
-                                    
-                                    // Refreshes the page after deleteion
-                                    header("Location: categories.php");
-                                    
-      
-                                }
-                                
-                                ?>
+                                <?php deleteCategories(); ?>
                                 <!-- DB Query to Delete Data ends-->
                                 
                                 
--- admin/functions.php
+++ admin/functions.php
@@ -40,6 +40,71 @@ function insertCategories(){
     }
 }
 
+function findAllCategories(){
+
+    // Must for function
+    global $connection;
+
+    $query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_categories)){
+        
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+
+        echo "<tr>";
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+
+        // Delete - Passing commant to GET - array returned - ([delete]=>$cat_id) 
+        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+
+        // Edit - Passing commant to GET - array returned - ([delete]=>$cat_id) 
+        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+        
+        echo "</tr>";
+    }
+
+}
+
+function deleteCategories(){
+    
+    // Must for function
+    global $connection;
+
+    // _GET looking for delete key
+    if(isset($_GET['delete'])){
+
+        
+                            
+        // Retreiving data from post
+        $the_cat_id = $_GET['delete'];
+        
+
+
+        $query = "DELETE FROM categories ";
+        $query .= "WHERE cat_id = {$the_cat_id} ";
+        $delete_query = mysqli_query($connection, $query);
+        
+        // Checks if the query fails - not printing if works - need to fix
+        // if(!$deletes_query){
+        //     die("QUERY FAILED" . mysqli_error($connection));
+        // }else{
+        //     echo "<font color='blue'>".$myvariable."</font>";
+        // }
+        
+        // Refreshes the page after deleteion
+        header("Location: categories.php");
+        
+
+    }
+
+
+
+
+}
+
 
 
 
