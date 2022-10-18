f297017bec78bd3b683f669c8961d4f23b730a4a
ARHoss <rhossaincse@gmail.com>
2022-10-18 15:42:13 -0600

    Modifying user table heading

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -2,15 +2,12 @@
     <thead>
         <tr>
             <th>Id</th>
-            <th>Author</th>
-            <th>Comment</th>
+            <th>Username</th>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Email</th>
-            <th>Status</th>
-            <th>In Response to</th>
-            <th>Date</th>
-            <th>Approve</th>
-            <th>Unapprove</th>
-            <th>Delete</th>
+            <th>User Role</th>
+            <th>Date Created</th>
         </tr>
     </thead>
 
