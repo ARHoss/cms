2748e944a83694248108c673dd2c2613ac7dfc2f
ARHoss <rhossaincse@gmail.com>
2022-10-21 17:59:06 -0600

    fixed

--- admin/functions.php
+++ admin/functions.php
@@ -12,6 +12,16 @@ function escape($string){
 
 }
 
+// Prints in Console Port
+function console_print($data) {
+    
+    if (is_array($data)){
+        $output = implode(',', $data);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }else{echo "<script>console.log('Debug Objects: " . $data . "' );</script>";}
+}
+
 
 function insertCategories(){
 
