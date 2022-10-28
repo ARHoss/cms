137028c5de15dbd1738e71d1039ab0b10b3c2e45
ARHoss <rhossaincse@gmail.com>
2022-10-28 13:19:51 -0600

    added new text to widget

--- includes/widget.php
+++ includes/widget.php
@@ -1,5 +1,7 @@
 <!-- Side Widget Well -->
 <div class="well">
     <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    <p>Please register to login and check-out the admin tab</p>
+    <p>You can email me using the contact tab</p>
+    <p>Thank you for visiting</p>
 </div>
