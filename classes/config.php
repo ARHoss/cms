e4a83b34092853ae68ff6e696fad2ebc4e08e14f
ARHoss <rhossaincse@gmail.com>
2022-11-03 11:29:53 -0600

    Updated - Sending email

---
+++ classes/config.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Config {
+
+
+    const SMTP_HOST     = 'smtp.mailtrap.io';                   //Set the SMTP server to send through
+    const SMTP_PORT     = 2525;                                 //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    const SMTP_USER     = '376d18bda4391c';                     //SMTP username
+    const SMTP_PASSWORD = 'fd15113f5c347f';                     //SMTP password
+
+
+
+}
+
+
+
+
+
+
+?>
--- forgot_password.php
+++ forgot_password.php
@@ -1,5 +1,14 @@
+<!-- PHP MAiler Configurations -->
+<?php use PHPMailer\PHPMailer\PHPMailer; ?>             <!-- Import PHPMailer classes into the global namespace -->
+<?php use PHPMailer\PHPMailer\SMTP;  ?>
+
+
 <?php  include "includes/db.php"; ?>
 <?php  include "includes/header.php"; ?>
+
+<!-- PHP MAiler Configurations -->
+<?php  include "./vendor/autoload.php"; ?>              <!-- Autoloads all the classes for PHP Mailer -->
+<?php  include "./classes/config.php"; ?>               <!-- PHP Mailer Custom Classes -->
 
 <?php 
     // Need to investigate
@@ -18,7 +27,7 @@
             $token = bin2hex(openssl_random_pseudo_bytes($length));
 
             // Checking for email and creating error_message
-            // Prepare Statements PHP
+            //-----------------------Prepare Statements PHP-----------------------//
             if(email_exists($user_email)){
 
                 // Sets up the connection
@@ -30,24 +39,56 @@
                     // Close the connection
                     mysqli_stmt_close($stmt);
                 } 
-                
-                
-                
-                
                 // Query testing 
                 // else {
                 //     echo mysqli_error($connection);
                 // }
 
 
+                //----------------------Configure PHP Mailer-------------------------------//
+
+                $mail = new PHPMailer(true);                                //Create an instance; passing `true` enables exceptions               
+                // echo get_class($mail);                                   // Checks if we received the class
+
+
+                
+                $mail->isSMTP();                                             //Send using SMTP
+                $mail->Host       = Config::SMTP_HOST;                       //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                    //Enable SMTP authentication
+                $mail->Username   = Config::SMTP_USER;                       //SMTP username
+                $mail->Password   = Config::SMTP_PASSWORD;                   //SMTP password
+                
+                // Mailtrap works with tls
+                $mail->SMTPSecure = 'tls';                                   //Enable implicit TLS encryption                                            
+                // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption //Not supported by mailtrap
+                
+                $mail->Port       = Config::SMTP_PORT;                       //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                $mail->isHTML(true);                                         //Set email format to HTML
+
+                //Recipients
+                $mail->setFrom('edwin@edwindiaz.com', 'Mailer');
+                $mail->addAddress($user_email);     //Add a recipient //Name is optional          
+                
+                // Content
+                $mail->Subject = 'Here is the subject';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                if($mail->send()){                                          // Send Email
+                    echo 'Message has been sent';
+                }else {
+                    echo "Message was not sent";
+                }                                          
+
+                
+                
+                
+                
+
+
 
             }
                 
-            
-
-
-
-
         }
     }
 
